430884721a79a872bc29d0984fe75d68663d6176
= <=>
2025-03-13 09:32:22 +0700

    Jobsheet 4 - Praktikum 2.2

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -33,9 +33,7 @@ class UserController extends Controller
         // UserModel::create($data);
 
         //coba akses model UserModel
-        $user = UserModel::findOr(2,['username', 'nama'], function(){
-            abort(404);
-        }); 
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
