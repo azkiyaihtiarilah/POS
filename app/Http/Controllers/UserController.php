b6a02e7d1f4937aeb490ec74f9c9c44c11a29a67
= <=>
2025-03-13 11:34:10 +0700

    Jobsheet 4 - Practicum 2.5  Attribute Changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -33,16 +33,49 @@ class UserController extends Controller
         // UserModel::create($data);
 
         //coba akses model UserModel
-        $user = UserModel::firstOrNew(
+        // $user = UserModel::create(
+        //     [
+        //         'username' => 'manager55',
+        //         'nama' => 'Manager55',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2
+        //     ]
+        // );
+        // $user->username= 'manager56';
+
+        // $user->isDirty(); // true
+        // $user->isDirty('username'); // true
+        // $user->isDirty('nama'); // false
+        // $user->isDirty('nama', 'username'); // true
+
+        // $user->isClean(); // false
+        // $user->isClean('username'); // false
+        // $user->isClean('nama'); // true
+        // $user->isClean('nama', 'username'); // false
+        
+        // $user->save();
+
+        // $user->isDirty(); // false
+        // $user->isClean(); // true
+        // dd($user->isDirty());
+
+        //coba akses model UserModel
+        $user = UserModel::create(
             [
-                'username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
+                'username' => 'manager11',
+                'nama' => 'Manager11',
                 'password' => Hash::make('12345'),
                 'level_id' => 2
             ]
         );
+        $user->username= 'manager12';
+
         $user->save();
 
-        return view('user', ['data' => $user]);
+        $user->wasChanged(); // true
+        $user->wasChanged('username'); // true
+        $user->wasChanged('nama'); // false
+        $user->wasChanged('nama', 'username'); // true
+        dd($user->wasChanged(['nama', 'username'])); //true
     }
 }
