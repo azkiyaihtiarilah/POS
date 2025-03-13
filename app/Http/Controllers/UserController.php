d3bdeb8597a5eb97ee9cce6bff1ae3ec0ace42bd
= <=>
2025-03-13 10:42:32 +0700

    Jobsheet 4 - Practicum 2.5

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -33,8 +33,16 @@ class UserController extends Controller
         // UserModel::create($data);
 
         //coba akses model UserModel
-        $user = UserModel::where('level_id')->count();
-        //dd($user);
+        $user = UserModel::firstOrNew(
+            [
+                'username' => 'manager33',
+                'nama' => 'Manager Tiga Tiga',
+                'password' => Hash::make('12345'),
+                'level_id' => 2
+            ]
+        );
+        $user->save();
+
         return view('user', ['data' => $user]);
     }
 }
