4b0c74868041331d6280ec55a1c4193645f2a76b
= <=>
2025-06-02 06:42:03 +0700

    Practicum 1 – Implementation of File Upload for data import

--- routes/web.php
+++ routes/web.php
@@ -112,6 +112,10 @@ Route::middleware(['authorize:ADM, MNG'])->group(function () {
         Route::get('/{id}/delete_ajax', [BarangController::class, 'confirm_ajax']);
         Route::delete('/{id}/delete_ajax', [BarangController::class, 'delete_ajax']);
         Route::delete('/{id}', [BarangController::class, 'destroy']);
+
+        //import
+        Route::get('/import', [BarangController::class, 'import']);
+        Route::post('/import_ajax', [BarangController::class, 'import_ajax']);
     });
 });
 
